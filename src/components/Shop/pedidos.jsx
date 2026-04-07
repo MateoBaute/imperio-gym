1064a72700d2f78f750bb1a47a5555448201dadb
MateoBaute <mateobaute10@gmail.com>
2026-04-07 17:20:35 -0300

    nombre del usuario

--- src/components/Shop/pedidos.jsx
+++ src/components/Shop/pedidos.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 
 export default function pedidos() {
     const [compras, setCompras] = useState([]);
+    const [usuario, setUsuario] = useState(null);
 
     async function traerCompras(){
         try {
@@ -19,11 +20,27 @@ export default function pedidos() {
     }
 
     async function datosUsuario(){
+        const usuarioId = localStorage.getItem('userId');
+        const idUsuarioInt = Number(usuarioId);
+        console.log(idUsuarioInt)
+
+        const body = {idUsuario: idUsuarioInt}
+
         try{
-            const response = await fetch('https://localhost:3001/datosUser');
+            const response = await fetch('https://backend-imperio.vercel.app/comprasUsuario', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            });
             const data = await response.json();
 
-            
+            if(data.success){
+                setUsuario(data.user)
+                console.log(usuario)
+            }
+
         }catch (error){
 
         }
@@ -31,6 +48,7 @@ export default function pedidos() {
 
     useEffect(()=> {
         traerCompras();
+        datosUsuario();
     }, [])
 
     return(
@@ -41,7 +59,7 @@ export default function pedidos() {
         ) : (
             compras.map((compra, index) => (
                 <div key={compra.id ?? `${compra.idUsuario ?? 'u'}-${compra.idProducto ?? 'p'}-${index}`}>
-                    <p><strong>Usuario:</strong> {compra.idUsuario}</p>
+                    <p><strong>Usuario:</strong> {usuario ? usuario.name : 'Usuario no encontrado'}</p>
                     <p><strong>Producto:</strong> {compra.idProducto}</p>
                     <p><strong>Fecha:</strong> {compra.fechaCompra}</p>
                 </div>
