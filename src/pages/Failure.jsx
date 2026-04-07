3fe5275770f96514aab9a6bedc57c01b91f735c9
Mateo <mateobaute10@gmail.com>
2026-04-07 11:08:50 -0300

    Failure detalles

--- src/pages/Failure.jsx
+++ src/pages/Failure.jsx
@@ -1,6 +1,6 @@
 export default function Failure() {
     return (
-        <div className="success-container">
+        <div className="failure-container">
             <h1>¡Pago Fallido!</h1>
             <p>Lo sentimos, tu pago no se pudo procesar.</p>
         </div>
